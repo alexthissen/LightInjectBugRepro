c3f2d9c919b61ee667f33b0b2be89f8e43cdd7e1
Alex Thissen <athissen@xpirit.com>
2019-09-05 08:48:50 +0200

    New repro for bug

---
+++ ASPNETCore30WebApp/CompositionRoot.cs
@@ -0,0 +1,26 @@
+﻿using LightInject;
+using System.Collections.Generic;
+
+namespace ASPNETCore30WebApp
+{
+    public class CompositionRoot : ICompositionRoot
+    {
+        public void Compose(IServiceRegistry serviceRegistry)
+        {
+            serviceRegistry.Register<IThing, Thing>(new PerRequestLifeTime());
+        }
+    }
+
+    public class Thing : IThing
+    {
+        public IEnumerable<string> ProduceValues()
+        {
+            return new[] { "value1", "value2" };
+        }
+    }
+
+    public interface IThing
+    {
+        IEnumerable<string> ProduceValues();
+    }
+}
